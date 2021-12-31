67e5f4cd0849a55703e73be744b43923d085624b
Erik D <62072464+erikd256@users.noreply.github.com>
2021-12-31 16:39:40 +0100

    Initial commit

---
+++ arch/x86/include/generated/asm/.syscalls_32.h.cmd
@@ -0,0 +1,1 @@
+cmd_arch/x86/syscalls/../include/generated/asm/syscalls_32.h := /bin/bash '/home/erikd/Dokumente/HaliumPorts/kernel_src/arch/x86/syscalls/syscalltbl.sh' /home/erikd/Dokumente/HaliumPorts/kernel_src/arch/x86/syscalls/syscall_32.tbl arch/x86/syscalls/../include/generated/asm/syscalls_32.h
